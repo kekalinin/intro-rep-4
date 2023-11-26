3575d31825ac09c9a28351193337e9a1c2b79aab
Kirill K. <k.e.kalinin@gmail.com>
2023-11-26 22:07:45 +0300

    Задача "Капучино"

--- main.py
+++ main.py
@@ -5,11 +5,34 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog
 
 
+class EditCoffee(QDialog):
+    def __init__(self):
+        super(EditCoffee, self).__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+
+    def accept(self) -> None:
+        con = sqlite3.connect('coffee.sqlite')
+        con.cursor().execute('INSERT INTO coffee(sort, stepen, v_zernah, vkus, price, volume) VALUES (?,?,?,?,?,?)',
+                             (self.SortEdit.text(),
+                              self.StepenEdit.text(),
+                              self.FormatEdit.text(),
+                              self.VkusEdit.text(),
+                              self.PriceEdit.text(),
+                              self.VolumeEdit.text()))
+        con.commit()
+        con.close()
+        self.done(0)
+
+    def reject(self) -> None:
+        self.done(0)
+
+
 class Espresso(QMainWindow):
     def __init__(self):
         super(Espresso, self).__init__()
         uic.loadUi("main.ui", self)
         self.load_coffee()
+        self.addCoffee.clicked.connect(self.add_cofee)
 
     def load_coffee(self):
         con = sqlite3.connect('coffee.sqlite')
@@ -23,6 +46,12 @@ class Espresso(QMainWindow):
             for j, stolbec in enumerate(stroka):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(stolbec)))
         con.close()
+
+    def add_cofee(self) -> None:
+        EDB = EditCoffee()
+        EDB.show()
+        EDB.exec()
+        self.load_coffee()
 
 
 app = QApplication(sys.argv)
